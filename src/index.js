c0ae8b1583dcc87cc6d8a564e83a13b6f12db364
jphilapy <jeff@altahost.com>
2021-01-06 13:00:14 -0500

    setup websockets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // require stuff
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express() // setup express
+const server = http.createServer(app)
+const io = socketio(server) // causes server to support web sockets
 
 const port = process.env.PORT || 3000 // setup port variable
 
@@ -10,11 +14,15 @@ const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
+io.on('connection', () => {
+    console.log('New websocket connection.')
+})
+
 app.get('/', (req, res) => {
     res.render(publicPath + 'index.html')
 })
 
 // server
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('server is up on port.' + port)
 })
