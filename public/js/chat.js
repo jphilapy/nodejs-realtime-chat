2f50d7df7625ad0e41d91fabc8074234f34938e7
jphilapy <jeff@altahost.com>
2021-01-07 09:00:52 -0500

    completed challenge

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,21 @@
-io()
+const socket = io()
+
+// socket.on('countUpdated', (count) => {
+//     console.log('the count has been updated.', count)
+//     // alert(count)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('clicked')
+//     socket.emit('increment')
+// })
+
+socket.on('message', (message) => {
+    console.log(message)
+    // alert(count)
+})
+
+document.querySelector('#send-message').addEventListener('click', () => {
+    console.log('clicked')
+    socket.emit('text', document.getElementById('text').value)
+})
--- src/index.js
+++ src/index.js
@@ -14,8 +14,23 @@ const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
-io.on('connection', () => {
+
+
+io.on('connection', (socket) => {
     console.log('New websocket connection.')
+
+    socket.emit('message', "Welcome!")
+
+    // socket.on('increment', () => {
+    //     count++
+    //     // socket.emit('countUpdated', count)
+    //     io.emit('countUpdated', count) // this allows us to update all clients connected to the site
+    // })
+
+
+    socket.on('text', (text) => {
+        io.emit('message', text) // this allows us to update all clients connected to the site
+    })
 })
 
 app.get('/', (req, res) => {
