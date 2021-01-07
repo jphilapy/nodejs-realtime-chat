b8f2065eaba51eb409b2a3c7e1688fe3b0a35093
jphilapy <jeff@altahost.com>
2021-01-07 15:14:43 -0500

    disabled buttons until process complete

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,10 @@
 const socket = io()
 
-
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocation = document.querySelector('#send-location')
 
 socket.on('message', (message) => {
     console.log(message)
@@ -9,8 +13,14 @@ socket.on('message', (message) => {
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -19,10 +29,14 @@ $messageForm.addEventListener('submit', (e) => {
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocation.addEventListener('click', () => {
+
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
+
+    // disable button till we get positive feedback
+    $sendLocation.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
@@ -30,6 +44,9 @@ document.querySelector('#send-location').addEventListener('click', () => {
             'longitude': position.coords.longitude
         }, () => {
             console.log('Location shared!')
+
+            // everything works good, enable the button again
+            $sendLocation.removeAttribute('disabled')
         })
     })
 })
