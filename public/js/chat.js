2cbd5f932492f9cae928179cb52cc04e78ddbbc4
jphilapy <jeff@altahost.com>
2021-01-08 08:47:58 -0500

    fixed issue with missing equal sign

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,7 +12,7 @@ const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-message-template').innerHTML
 
 // Options
-const { username, room } Qs.parse(location.search, { ignoreQueryPrefix: true })
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
 socket.on('message', (message) => {
     console.log(message)
